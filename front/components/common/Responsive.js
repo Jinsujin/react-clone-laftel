5684ee8d27817ebb5d7349c324542e7bb2c9b2b3
Jinsujin <sylpid003@gmail.com>
2020-08-29 08:43:55 +0900

    홈 - 캐러셀 디자인 수정, slider custom button

--- front/components/common/Responsive.js
+++ front/components/common/Responsive.js
@@ -6,6 +6,7 @@ import styled from 'styled-components';
  */
 
 const ResponsiveBlock = styled.div`
+  position: relative;
   width: 1200px;
   margin: 0 auto;
 
--- front/components/home/Carousel.js
+++ front/components/home/Carousel.js
@@ -1,31 +1,28 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import Slider from 'react-slick';
 import styled, { createGlobalStyle } from 'styled-components';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 
 const GlobalStyled = createGlobalStyle`
-.slick-slider {
-    position: relative;
-}
-.slick-slide {
-    display: inline-block;
-}
-.slick-arrow {
-    position: absolute;
-    left: 0;
-    top: 50%;
-    z-index: 99;
-}
-.slick-prev {}
-.slick-next {right: 0; left: initial;}
+  .slick-slider {
+      position: relative;
+  }
+  .slick-slide {
+      display: inline-block;
+  }
+  .slick-arrow { 
+      display: none !important;
+  }
 `;
 
 const SlickWrapper = styled.div`
+  position: relative;
   height: calc(100% - 44px);
   margin-bottom: 3rem;
 `;
 
 const ImgWrapper = styled.div`
-  padding: 0px 10px;
+  /* padding: 0px 10px; */
   text-align: center;
 
   & img {
@@ -35,7 +32,44 @@ const ImgWrapper = styled.div`
   }
 `;
 
+const ArrowButton = styled.div`
+  z-index: 99;
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 10%;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: transparent;
+  background-color: rgba(0, 0, 0, 0);
+  transition: all 0.5s ease-in;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+  & span {
+    color: #fff;
+    opacity: 0.8;
+    font-size: 4em;
+    transition: all 0.3s ease-in;
+  }
+  &:hover span {
+    opacity: 1;
+  }
+`;
+
 const Carousel = ({ images }) => {
+  const sliderEl = useRef();
+  const onPrev = useCallback(() => {
+    sliderEl.current.slickPrev();
+  }, []);
+
+  const onNext = useCallback(() => {
+    sliderEl.current.slickNext();
+  }, []);
+
   const settings = {
     dots: false,
     infinite: true,
@@ -48,13 +82,19 @@ const Carousel = ({ images }) => {
     <>
       <GlobalStyled />
       <SlickWrapper>
-        <Slider {...settings}>
+        <ArrowButton onClick={onPrev}>
+          <LeftOutlined />
+        </ArrowButton>
+        <Slider ref={sliderEl} {...settings}>
           {images.map(v => (
             <ImgWrapper key={v.imageSrc}>
               <img src={v.imageSrc} alt={v.imageSrc} />
             </ImgWrapper>
           ))}
         </Slider>
+        <ArrowButton onClick={onNext} style={{ left: 'initial', right: '0' }}>
+          <RightOutlined />
+        </ArrowButton>
       </SlickWrapper>
     </>
   );
