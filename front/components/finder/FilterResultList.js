65d23c3815c00faa88e2e21b123392e0211836ea
Jinsujin <sylpid003@gmail.com>
2020-08-29 12:55:23 +0900

    태그검색페이지 카드 컴포넌트 레이아웃 변경

--- front/components/finder/FilterResultList.js
+++ front/components/finder/FilterResultList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Row, Col, Card } from 'antd';
+import FinderCard from './FinderCard';
 
 const FilterResultListWrapper = styled.div`
   margin-top: 3rem;
   margin-left: 260px;
   width: calc(100% - 260px);
-  padding-left: 2rem;
+  /* overflow: hidden; */
 
   p {
     color: #494c62;
     font-size: 1.5rem;
     margin-bottom: 2rem;
+    margin-left: 2rem;
 
     span {
       font-size: 1.7rem;
@@ -21,31 +23,29 @@ const FilterResultListWrapper = styled.div`
   }
 `;
 
+const CardWrap = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
 const FilterResultList = ({ animationItems }) => {
   return (
     <FilterResultListWrapper>
       <p>
         총 <span>5,909</span>개의 작품이 검색되었어요!
       </p>
-      <section>
-        <Row gutter={24}>
-          <Col span={8}>
-            <Card title="Card title" bordered={false}>
-              Card content
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Card title" bordered={false}>
-              Card content
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Card title" bordered={false}>
-              Card content
-            </Card>
-          </Col>
-        </Row>
-      </section>
+      <CardWrap>
+        {/* {animationItems.map(v => (
+          <ImageCard animation={v} />
+        ))} */}
+        <FinderCard />
+        <FinderCard />
+        <FinderCard />
+        <FinderCard />
+        <FinderCard />
+        <FinderCard />
+        <FinderCard />
+      </CardWrap>
     </FilterResultListWrapper>
   );
 };
---
+++ front/components/finder/FinderCard.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ImageCardWrap = styled.div`
+  cursor: pointer;
+  position: relative;
+  margin: 1.25rem 2rem;
+  width: calc((100% - 16rem) / 4);
+
+  & .thumbnail {
+    position: relative;
+  }
+  & > img {
+    border-radius: 4px;
+    display: block;
+    object-fit: cover;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: #e9ecef;
+    border: none;
+  }
+  & > .title {
+    font-size: 1.2rem;
+    line-height: 1.5rem;
+    color: #494c62;
+    margin-top: 0.25rem;
+    font-weight: 500;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow-x: hidden;
+  }
+`;
+
+const FinderCard = () => {
+  return (
+    <ImageCardWrap>
+      <div className="thumbnail">
+        <img
+          src="https://image.laftel.net/items/thumbs/big/9940a153-7a77-4ce9-bdf4-a714756b8c3e.jpg"
+          alt=""
+        />
+      </div>
+      <div className="title">암살교실 2기</div>
+    </ImageCardWrap>
+  );
+};
+
+export default FinderCard;
